ea73bc165352df2b6f4285d2fe0e6bb68964fcc7
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2023-07-12 22:06:28 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,15 +2,24 @@
 #include <math.h>
 using namespace std;
 
+long long powpow (int base, int power) {
+	long long answer=1;
+	for(int i=0; i<power;++i){
+		answer*=base;
+		answer%=1234567891; 
+	}
+	return answer;
+}
+
 int main() {
 	
 	int L; cin >> L;
 	string str; cin >> str;
 	int size = str.size();
-	int answer = 0;
+	long long answer = 0;
 	for(int i=0; i<size; ++i) {
-		int num = (int)str[i]-(int)'a'+1;
-		answer+=num*pow(31,i);
+		long long num = (int)str[i]-(int)'a'+1;
+		answer+=num*powpow(31,i);
 	}
 	cout << answer << '\n';
 	
