3b955f0d27508063db7f3a57bd6c8813ba2137cf
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2023-07-12 22:20:52 +0900

    [Silver IV] Title: solved.ac, Time: 84 ms, Memory: 5108 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
+#include <math.h>
 using namespace std;
-int n,arr[300000+5];
-double sum;
-int main(){
-	cin>>n;
-	if(!n){
-		cout<<0;
-		return 0;
+
+int main() {
+	
+	int L; cin >> L;
+	if(L==0){
+		cout << 0 << '\n';
+		exit(0);
 	}
-	for(int i=0;i<n;i++){
-		cin>>arr[i];
-	}
-	sort(arr, arr+n);
-	int trim = round(0.15*n);
-	for(int i=trim; i<n-trim; i++){
-		sum += arr[i];
-	}
-	cout<<round(sum/(n-2*trim));
+	vector<int>vec;
+	while(L--){
+		int n; cin >> n;
+		vec.push_back(n);
+	}	
+	sort(vec.begin(),vec.end());
+	int size = vec.size();
+	size=round(size*0.15);
+	double mean = 0;
+	for(int i = size; i<vec.size()-size; ++i)
+		mean+=vec[i];
+		
+	cout << round(mean/(vec.size()-2*size)) << '\n';
+	
 	return 0;
 }
