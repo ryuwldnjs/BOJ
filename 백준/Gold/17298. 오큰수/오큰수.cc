f4df732c638ed6fae9c50c1025a59ab4bc4a029a
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-01-27 22:50:44 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 13544 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+stack<int> s;
+int n,arr[1000000+50];
+vector<int> answer;
+int main(){
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	for(int i=n-1;i>=0;i--){
+		while(!s.empty() && s.top() <= arr[i])
+			s.pop();
+		if(s.empty()) answer.push_back(-1);
+		else answer.push_back(s.top());
+		
+		s.push(arr[i]);
+	}
+	
+	for(int i=answer.size()-1; i>=0; i--){
+		cout<<answer[i]<<' ';
+	}
+	return 0;
+}
