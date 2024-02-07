a2c1b838eeb21ef5e40203dc5387325bd0bcfd23
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-07 15:14:11 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,17 +1,20 @@
 #include <vector>
-#include <algorithm>
-#include <cstdio>
+// #include <algorithm>
+// #include <cstdio>
+#include <set>
 using namespace std;
 
 int solution(vector<int> nums)
 {
-    int answer = 0;
-    int n = nums.size();
-    sort(nums.begin(), nums.end());
-    // auto tmp = unique(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
     
-    answer = min((int)nums.size(), n/2);
-    // printf("%d, %d", )
-    return answer;
+    // int answer = 0;
+//     int n = nums.size();
+//     sort(nums.begin(), nums.end());
+//     // auto tmp = unique(nums.begin(), nums.end());
+//     nums.erase(unique(nums.begin(), nums.end()), nums.end());
+    
+//     answer = min((int)nums.size(), n/2);
+//     // printf("%d, %d", )
+    set<int> s(nums.begin(), nums.end());
+    return min(s.size(), nums.size() / 2);
 }
