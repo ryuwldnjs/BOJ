51048b18f7ac1da8fcd6b5fd0d46611d820a8a4c
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-10 02:42:42 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -11,17 +11,9 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
     
     for(int i=1;i<=n;i++){
         if(have[i] == 0){
-            if(have[i-1] == 2){
-                have[i-1]--;
-                have[i]++;
-            }
-            else if(have[i+1] == 2){
-                have[i+1]--;
-                have[i]++;
-            }
-            else{
-                answer--;
-            }
+            if(have[i-1] == 2) have[i-1] = have[i] = 1;
+            else if(have[i+1] == 2) have[i+1] = have[i] = 1;
+            else answer--;
         }
     }
     return answer;
