309a2e5eb152c26d55af6dbe4d6ebae12bb90052
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-12 03:25:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.23 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using pii=pair<int,int>;
+queue<pii> q;
+int n,m;
+int dx[4] = {0,0,-1,1};
+int dy[4] = {-1,1,0,0};
+
+bool pass(int x, int y, vector<vector<int>>&maps){
+    return 0 <=x&&x< n && 0 <=y&&y< m && maps[x][y];
+}
+
+int solution(vector<vector<int> > maps){
+    n = maps.size();
+    m = maps[0].size();
+    vector<vector<int>> dist(n, vector<int>(m));
+    
+    q.push({0,0});
+    dist[0][0] = 1;
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        for(int i=0;i<4;i++){
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            
+            if(pass(xx, yy, maps) && dist[xx][yy] == 0){
+                dist[xx][yy] = dist[x][y] + 1;
+                q.push({xx,yy});
+            }
+        }
+        
+    }
+    return dist[n-1][m-1] ? dist[n-1][m-1] : -1;
+}
