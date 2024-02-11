de652c8718786140984a3acccf07cbf403a4d956
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-12 00:36:12 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 3459.42 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> answer(27);
+int arr[27];
+string getCourse(int n){
+    string course = "";
+    for(int i=0;i<26;i++){
+        if(n & (1<<i)) course += 'A' + i;
+    }
+    return course;
+}
+int getBit(string s){
+    int tmp = 0;
+    for(auto c: s) tmp += 1<<(c - 'A');
+    return tmp;
+}
+int getBitCnt(int n){
+    int cnt = 0;
+    while(n){
+        if(n&1) cnt++;
+        n >>= 1;
+    }
+    return cnt;
+}
+
+void solve(int order, int k, vector<string> &orders){
+    int bits = getBitCnt(order);
+    if(bits > 10) return;
+    
+    if(k == 26){
+        int cnt = 0; // 해당 조합을 포함하는 주문들 개수
+        for(int i=0;i<orders.size();i++){
+            if((getBit(orders[i]) & order) == order) cnt++;
+        }
+        
+        if(arr[bits] < cnt && cnt >= 2){ // 더 많이 해당되는 조합 발견
+            arr[bits] = cnt;
+            answer[bits] = vector<int>(1, order);
+            // cout<<order<<' ';
+        }
+        else if(arr[bits] == cnt && cnt >= 2){
+            answer[bits].push_back(order);
+        }
+        
+        
+        return;
+    }
+    solve(order | (1<<k), k+1, orders);
+    solve(order, k+1, orders);
+}
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answerString;
+    solve(0, 0, orders);
+
+    for(int cnt : course){
+        for(int j=0;j<answer[cnt].size();j++){
+            answerString.push_back(getCourse(answer[cnt][j]));
+        }
+    }
+    sort(answerString.begin(), answerString.end());
+    return answerString;
+}
