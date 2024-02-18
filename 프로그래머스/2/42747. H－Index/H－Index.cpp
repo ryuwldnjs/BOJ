f2eb9ad387831515fe2cdd956fd91b2d8a489d0f
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-18 23:04:07 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    for(int i=0;i<citations.size();i++){
+        if(citations[i] >= i+1) answer = i+1;
+    }
+    return answer;
+}
