a982b534216b36af4c0ff0f7256dcb1f42657eeb
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-23 18:46:58 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 11.54 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll=long long;
+int solution(vector<int> food_times, ll k) {
+    vector<int> sorted_time = food_times;
+    sort(sorted_time.begin(), sorted_time.end());
+    
+    ll turn = 0;
+    ll total_time = 0;
+    for(int i=0;i<sorted_time.size();i++){
+        // printf("%d %d, ", turn, total_time);
+        ll remain = sorted_time.size() - i;
+        ll cost = remain * (sorted_time[i] - turn);
+        
+        if(total_time + cost <= k){
+            total_time += cost;
+            turn += sorted_time[i] - turn;
+        }
+        else {
+            total_time += (k - total_time) / remain * remain;
+            turn += (k - total_time) / remain;
+            break;
+        }
+        
+    }
+    
+        // printf("%d %d, ", turn, total_time);
+    for(int i=0;i<food_times.size();i++){
+        if(food_times[i] > turn && total_time++ == k) return i+1;
+    }
+    return -1;
+}
