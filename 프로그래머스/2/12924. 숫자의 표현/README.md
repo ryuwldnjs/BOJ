dcc630a2f0121732e6b57695ff5e2c367d6d3a52
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-27 17:43:56 +0900

    [level 2] Title: 숫자의 표현, Time: 0.27 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#qna) 
 
 ### 성능 요약
 
-메모리: 3.92 MB, 시간: 0.08 ms
+메모리: 3.81 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 27일 17:09:17
+2024년 02월 27일 17:43:55
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -6,14 +6,16 @@ int solution(int n) {
     vector<int> psum(10000+1, 0);
     int answer = 0;
     for(int i=1;i<10000+1;i++) psum[i] = psum[i-1] + i;
-    
-    int l = 0;
-    int r = 1;
-    while(l<r){
-        int sum = psum[r] - psum[l];
-        if(sum == n) answer++;
-        if(sum >= n) l++;
-        else r++;
+    for(int i=0;i<10000+1;i++){
+        if(binary_search(psum.begin(), psum.end(), n+psum[i])) answer += 1;
     }
+    // int l = 0;
+    // int r = 1;
+    // while(l<r){
+    //     int sum = psum[r] - psum[l];
+    //     if(sum == n) answer++;
+    //     if(sum >= n) l++;
+    //     else r++;
+    // }
     return answer;
 }
