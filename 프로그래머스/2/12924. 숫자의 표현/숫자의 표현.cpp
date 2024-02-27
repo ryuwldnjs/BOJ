c6d54a9b92634e4f5ae41a1786754500be3a879f
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-27 17:52:46 +0900

    [level 2] Title: 숫자의 표현, Time: 0.29 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,21 +1,14 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int solution(int n) {
     vector<int> psum(10000+1, 0);
     int answer = 0;
+    int a=0;
     for(int i=1;i<10000+1;i++) psum[i] = psum[i-1] + i;
+    for(int i=0;i<5000000;i++) a++;
     for(int i=0;i<10000+1;i++){
-        if(binary_search(psum.begin(), psum.end(), n+psum[i])) answer += 1;
+        if(binary_search(psum.begin(), psum.end(), n+psum[i])) answer++;
     }
-    // int l = 0;
-    // int r = 1;
-    // while(l<r){
-    //     int sum = psum[r] - psum[l];
-    //     if(sum == n) answer++;
-    //     if(sum >= n) l++;
-    //     else r++;
-    // }
     return answer;
 }
