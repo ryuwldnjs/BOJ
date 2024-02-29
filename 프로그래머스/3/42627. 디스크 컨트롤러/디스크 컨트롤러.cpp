97250bf1338cf6d2ac12c2f9287056c04fd37f87
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-29 16:49:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct info{
+    int start;
+    int time;
+    const bool operator< (const info& tmp) const{
+        return time > tmp.time;
+    }
+};
+
+int solution(vector<vector<int>> jobs) {
+    sort(jobs.begin(), jobs.end());
+    priority_queue<info> pq;
+    int answer = 0;
+    int last = jobs[0][0];
+    int idx = 1;
+    pq.push({jobs[0][0], jobs[0][1]});
+    
+    while(true){
+        if(pq.empty() && idx == jobs.size()) break;
+        if(pq.empty()){ //겹치지 않는 스케줄일때
+            pq.push({jobs[idx][0], jobs[idx][1]});
+            last = jobs[idx][0];
+            idx++;
+        }
+        auto top = pq.top();
+        printf("[%d,%d] ",top.start, top.time);
+        pq.pop();
+        
+        while(idx < jobs.size()){
+            if(jobs[idx][0] <= last + top.time){
+                pq.push({jobs[idx][0], jobs[idx][1]});
+                idx++;
+            }
+            else break;
+        }
+        last += top.time;
+        answer += last - top.start; //각 스케줄의 총 소요시간 합산
+        printf(", %d %d", last, answer);
+    }
+    cout<<answer;
+    return answer / jobs.size();
+}
