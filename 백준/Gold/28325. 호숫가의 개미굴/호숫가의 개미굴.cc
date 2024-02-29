4e2136259e9a3fa67fc4296be1b60c18e0a39739
柳 <69042677+ryuwldnjs@users.noreply.github.com>
2024-02-29 18:26:47 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 192 ms, Memory: 23504 KB, Score: 35 point -BaekjoonHub

---
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+ll n, arr[250000+5];
+ll dp[2500000+5];
+ll answer, sum;
+int main(){
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+		sum += arr[i];
+	}
+	
+	//마지막 방에는 안산다고 가정 
+	dp[0] = !arr[0];
+	dp[1] = arr[1] || dp[0] ? 0 : 1;
+	
+	for(int i=2;i<n-1;i++){
+		if(arr[i]) dp[i] = dp[i-1];
+		else if(arr[i-1]) dp[i] = dp[i-1] + 1;
+		else dp[i] = max(dp[i-2] + 1, dp[i-1]);
+	}
+	answer = dp[n-2];
+	
+	//첫번째 방에는 안산다고 가정 
+	dp[0] = 0;
+	dp[1] = !arr[1];
+	
+	for(int i=2;i<n;i++){
+		if(arr[i]) dp[i] = dp[i-1];
+		else if(arr[i-1]) dp[i] = dp[i-1] + 1;
+		else dp[i] = max(dp[i-2] + 1, dp[i-1]);
+	}
+	
+	answer = max(answer, dp[n-1]);
+	cout<<answer + sum;
+	return 0;
+} 
