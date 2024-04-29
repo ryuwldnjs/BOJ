a8c4e31b5097fa53213b6e819278b93f4cfec117
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2024-04-29 14:09:57 +0900

    [Gold IV] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+vector<int> lis;
+int n,a;
+int main(){
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a;
+		auto target = lower_bound(lis.begin(), lis.end(), a);
+		if(target == lis.end()) lis.push_back(a);
+		else *target = a;
+	}
+	cout<<n - lis.size();
+	return 0;
+}
