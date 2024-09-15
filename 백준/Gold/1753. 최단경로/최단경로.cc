cddcbd2149b933d0d5640993a49e42699d7cbf13
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2024-09-15 19:10:35 +0900

    [Gold IV] Title: 최단경로, Time: 276 ms, Memory: 9076 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,70 +1,59 @@
-#include <stdio.h>
+/**
+ * https://www.acmicpc.net/problem/1753
+ * 최단경로 : G4
+ * 다익스트라
+ */
+#include <iostream>
 #include <vector>
 #include <queue>
-#define INF 123456789
+#define INF 1e9
 using namespace std;
+using pii=pair<int,int>;
+
+vector<vector<pii>> graph; //간선정보 
+priority_queue<pii, vector<pii>, greater<pii>> pq;
+vector<int> dist;
 int v,e,k; //정점갯수 ,간선 갯수, 시작 점 
-int from, to, cost; //from에서 to로 가중치는 cost 
-vector<vector<pair<int,int> > > a; //간선정보 
-priority_queue<pair<int, int> > pq;//우선순위 큐 
-int dist[20000+50];
 
-void dijkstra(int start)
-{
-	for(int i=1;i<=v;i++) //초기거리 무한대 설정 
-		dist[i]=INF;
-		
-		
-	dist[start]=0; 
-	pq.push({ -dist[start], start });
+void dijkstra(int start){
+	for(int i=1;i<=v;i++) dist[i] = INF;//초기거리 무한대 설정 
+	dist[start] = 0;
+	pq.push({ dist[start], start }); //pair타입이라, 1번째 인자에 거리를 넣어야함
 	
-	while(!pq.empty()) //큐가 빌때까지
-	{
-
-		int distance = -pq.top() .first; //거리는 음수로 저장 
- 		int current = pq.top().second;
-		pq.pop(); //팝!
+	while(!pq.empty()){
+		int distance = pq.top().first;
+ 		int now = pq.top().second;
+		pq.pop();
 		
-		if(dist[current] < distance) //이미 더 짧은 경로일경우 
-			continue; 
+		if(dist[now] < distance) continue; //이미 더 짧은 경로가 있으면, 무시함
 		
-		
-		for(int i=0; i<a[current].size() ; i++) //current에서 연결된 정점들 체크 
-		{
-			int next = a[current][i].first; //다음타겟 current에서 i로 가는 경로 
-			int next_distance = distance + a[current][i].second; //다음 타겟의 가중치 
+		for(int i=0; i<graph[now].size() ; i++){
+			int next = graph[now][i].first; //다음타겟  
+			int next_distance = distance + graph[now][i].second; //다음 타겟의 가중치 
 			
-			if(next_distance <dist[next])
-			{
+			if(next_distance < dist[next]){
 				dist[next] = next_distance; 
-				pq.push({-next_distance, next});
+				pq.push({next_distance, next});
 			}
-		 }
-		 
-		  
-	 } 
+		}
+	}
+
 }
 
+int main(){
+	cin>>v>>e>>k;
+	graph.resize(v+1); dist.resize(v+1);
 
-int main()
-{
-	scanf("%d %d %d", &v,&e, &k);
-	a.resize(v+1);
-	for(int i=1;i<=e;i++)
-	{
-		scanf("%d %d %d", &from,&to,&cost);
-		a[from].push_back({to,cost});
+	for(int i=0;i<e;i++){
+		int from, to, cost;
+		cin>>from>>to>>cost;
+		graph[from].push_back({to,cost});
 	 } 
+
 	 dijkstra(k);
-	// printf("다익스트라 탈출!!!!");
-	
-	
-	 for(int i=1;i<=v;i++)
-	 {
-	 	if(dist[i]!=INF)
-	 		printf("%d\n", dist[i]);
-	 	else
-	 		printf("INF\n");
+	 for(int i=1;i<=v;i++){
+		if(dist[i] == INF) cout<<"INF\n";
+		else cout<<dist[i]<<'\n';
 	 }
 	return 0;
 }
