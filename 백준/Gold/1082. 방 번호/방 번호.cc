e755ac5500d01dd02981e7accc6b8302c57c66c8
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2024-09-29 02:26:22 +0900

    [Gold III] Title: 방 번호, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1082. 방 번호/방 번호.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, cost[10], m;
+string MAX;
+
+string dp[55];
+// dp[i] : i원일때의 최대 방번호
+
+bool cmp(string a, string b){
+    if(a.size()==b.size()){
+        for(int i=0;i<a.size();i++){
+            if(a[i] != b[i]) return a[i] < b[i];
+        }
+    }
+    return a.size() < b.size();
+}
+int main(){
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>cost[i];
+    }
+    cin>>m;
+    for(int i=1;i<=m;i++){
+        for(int j=0;j<n;j++){
+            string tmp;
+            if(i - cost[j] >= 0) tmp = dp[i - cost[j]] + to_string(j);
+            sort(tmp.begin(), tmp.end(), greater<char>());
+            if(tmp.front()=='0') continue;
+            // if(dp[i].size() <= tmp.size() && tmp.size() && tmp.back() != '0'){
+            //     dp[i] = max(dp[i], tmp, cmp);
+            // }
+                dp[i] = max(dp[i], tmp, cmp);
+
+        }
+        // cout<<i<<' '<<dp[i]<<'\n';
+        sort(dp[i].begin(), dp[i].end(), greater<char>());
+    }
+    for(int i=1;i<=m;i++){
+        if(dp[i].size()==0) continue;
+        MAX = max(MAX, dp[i], cmp);
+        // cout<<i<<' '<<dp[i]<<'\n';
+    }
+    if(MAX == "") MAX = "0";
+    cout<<MAX;
+    return 0;
+}
