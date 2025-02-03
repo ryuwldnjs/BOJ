5d9ef2eb049b6365c607246f4e23b590a0208145
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-02-02 22:37:18 -0800

    [Platinum IV] Title: 단절점, Time: 20 ms, Memory: 4464 KB -BaekjoonHub

--- 백준/Platinum/11266. 단절점/단절점.cc
+++ 백준/Platinum/11266. 단절점/단절점.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 vector<vector<int>> graph;
-vector<int> discovered, res, ans;
+vector<int> discovered;
+set<int> ans;
 int v,e, cnt;
 int dfs(int now, bool isRoot){
     discovered[now] = ++cnt;
@@ -16,16 +18,16 @@ int dfs(int now, bool isRoot){
         int next = graph[now][i];
         if(discovered[next] == 0){
             child++;
-            int low = dfs(next, false);
-            num = min(num, low);
-            if(!isRoot && low >= discovered[now]) res[now] = true;
+            int subtree = dfs(next, false);
+            num = min(num, subtree);
+            if(!isRoot && subtree >= discovered[now]) ans.insert(now);
         }
         else{
             num = min(num, discovered[next]);
         }
     }
     //루트노드 - 자식이 2개 이상이면 단절점
-    if(isRoot && child > 1) res[now] = true;
+    if(isRoot && child > 1) ans.insert(now);
     return num;
 }
 int main(){
@@ -34,7 +36,6 @@ int main(){
     cin>>v>>e;
     graph.resize(v+1);
     discovered.resize(v+1);
-    res.resize(v+1);
     for(int i=0;i<e;i++){
         int a,b; cin>>a>>b;
         graph[a].push_back(b);
@@ -43,9 +44,7 @@ int main(){
     for(int i=1;i<=v;i++){
         if(discovered[i] == 0) dfs(i, true);
     }
-    for(int i=1;i<=v;i++){
-        if(res[i]) ans.push_back(i);
-    }
+
     cout<<ans.size()<<'\n';
     for(auto i: ans) cout<<i<<' ';
     return 0;
