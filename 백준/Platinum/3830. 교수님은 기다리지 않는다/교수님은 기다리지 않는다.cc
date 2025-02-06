378c931ebc539169c5ba63958bdaa4c3bcc55cb5
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-02-06 17:12:22 +0900

    [Platinum III] Title: 교수님은 기다리지 않는다, Time: 412 ms, Memory: 5276 KB -BaekjoonHub

--- 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
+++ 백준/Platinum/3830. 교수님은 기다리지 않는다/교수님은 기다리지 않는다.cc
@@ -1,33 +1,27 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-struct Info{
-    int idx, cost;
-};
+
 vector<int> parent, dist;
 int n;
-Info find(int x){
-    if(parent[x] < 0) return {x, dist[x]};
-    Info tmp = find(parent[x]);
-    parent[x] = tmp.idx;
-    dist[x] += tmp.cost;
-    return {parent[x], dist[x]};
+int find(int x){
+    if(parent[x] < 0) return x;
+    int p = find(parent[x]);
+    dist[x] += dist[parent[x]];
+    return parent[x] = p;
 }
 int merge(int a, int b, int c){
-    int p_a = find(a).idx;
-    int p_b = find(b).idx;
+    int p_a = find(a);
+    int p_b = find(b);
     if(p_a == p_b) return 0;
     parent[p_a] += parent[p_b];
     parent[p_b] = p_a;
-    // c -= dist[b];
+    
     dist[p_b] = c + dist[a] - dist[b];
     return 1;
 }
 void query(int a, int b){
-    int p_a = find(a).idx;
-    int p_b = find(b).idx;
-
-    if(p_a == p_b){
+    if(find(a) == find(b)){
         cout<<dist[b] - dist[a]<<'\n';
     }
     else{
