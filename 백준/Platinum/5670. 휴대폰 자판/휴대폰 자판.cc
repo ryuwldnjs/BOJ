e80cdac8104b781894b035fe7c34be285420b67c
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-02-12 20:25:55 +0900

    [Platinum IV] Title: 휴대폰 자판, Time: 836 ms, Memory: 186324 KB -BaekjoonHub

---
+++ 백준/Platinum/5670. 휴대폰 자판/휴대폰 자판.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+struct Node{
+    unordered_map<char, Node*> children;
+    bool isEndOfWord;
+};
+struct Trie{
+    Node* root;
+    Trie(){
+        root = new Node();
+    }
+
+    void insert(string& word){
+        Node* node = root;
+        for(char ch: word){
+            if(node->children.count(ch) == 0){
+                node->children[ch] = new Node();
+            }
+            node = node->children[ch];
+        }
+        node->isEndOfWord = true;
+    }
+
+    int getTrialCount(string& word){
+        Node* node = root;
+        int cnt = 0;
+        for(int i=0;i<word.size();i++){
+            char ch = word[i];
+            if(i==0 || node->children.size() > 1 || node->isEndOfWord == true){ // 자식이 하나뿐일때
+                cnt++;            
+            }
+            node = node->children[ch];
+        }
+        return cnt;
+    }
+};
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    int n;
+    cout<<fixed; cout.precision(2);
+    while(cin>>n){
+        Trie trie;
+        vector<string> words;
+        double answer = 0;
+        for(int i=0;i<n;i++){
+            string word; cin>>word;
+            trie.insert(word);
+            words.push_back(word);
+        }
+        for(string word: words){
+            answer += trie.getTrialCount(word);
+        }
+        answer /= n;
+        cout<<answer<<'\n';
+
+    }
+    return 0;
+}
