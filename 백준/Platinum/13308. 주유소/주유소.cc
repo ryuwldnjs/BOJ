781f70976e3a12a7617f270787d4d806edc39e4f
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-02-24 17:08:23 +0900

    [Platinum V] Title: 주유소, Time: 1812 ms, Memory: 260712 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/13308. 주유소/주유소.cc
+++ 백준/Platinum/13308. 주유소/주유소.cc
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define INF 1e9
+#define INF 1e18
 using namespace std;
+using ll=long long;
+
 struct Info{
-    int b,c;
+    ll b,c;
     bool operator<(const Info& other)const{
         return c > other.c;
     }
 };
 
-int n,m;
+ll n,m;
 vector<vector<Info>> graph, new_graph;
-vector<vector<int>> dist;
-vector<int> price;
-void dijkstra(int start, vector<vector<Info>>& graph, vector<int>& dist){
+vector<vector<ll>> dist;
+vector<ll> price;
+void dijkstra(ll start, vector<vector<Info>>& graph, vector<ll>& dist){
     priority_queue<Info> pq;
     dist[start] = 0;
     pq.push({start, 0});
 
     while(!pq.empty()){
-        int now = pq.top().b;
-        int now_distance = pq.top().c;
+        ll now = pq.top().b;
+        ll now_distance = pq.top().c;
         pq.pop();
 
         if(dist[now] < now_distance) continue;
 
-        for(int i=0;i<graph[now].size();i++){
-            int next = graph[now][i].b;
-            int next_distance = now_distance + graph[now][i].c;
+        for(ll i=0;i<graph[now].size();i++){
+            ll next = graph[now][i].b;
+            ll next_distance = now_distance + graph[now][i].c;
 
             if(dist[next] <= next_distance) continue;
             dist[next] = next_distance;
@@ -43,27 +45,30 @@ int main(){
     cin>>n>>m;
     graph.resize(n+1);
     new_graph.resize(n+1);
-    dist.resize(n+1, vector<int>(n+1, INF));
+    dist.resize(n+1, vector<ll>(n+1, INF));
     price.resize(n+1);
 
-    for(int i=1;i<=n;i++){
+    for(ll i=1;i<=n;i++){
         cin>>price[i];
     }
-    for(int i=0;i<m;i++){
-        int a,b,c; cin>>a>>b>>c;
+    for(ll i=0;i<m;i++){
+        ll a,b,c; cin>>a>>b>>c;
         graph[a].push_back({b,c});
         graph[b].push_back({a,c});
     }
     
-    for(int i=1;i<=n;i++){
+    for(ll i=1;i<=n;i++){
         dijkstra(i, graph, dist[i]);
-        for(int j=1;j<=n;j++){
+        for(ll j=1;j<=n;j++){
             if(i==j) continue;
             new_graph[i].push_back({j, price[i] * dist[i][j]});
         }
     }
-    vector<int> answer(n+1, INF);
+
+
+    vector<ll> answer(n+1, INF);
     dijkstra(1, new_graph, answer);
+
     cout<<answer[n];
     return 0;
 }
