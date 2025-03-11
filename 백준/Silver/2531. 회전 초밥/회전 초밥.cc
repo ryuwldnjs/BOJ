49d8358994c2a581643f7fa412cbae5140caf30f
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-03-11 19:47:15 +0900

    [Silver I] Title: 회전 초밥, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int n,d,k,c;
-int now, MAX;
-vector<int> conveyer, sushi(3000+5);
 int main(){
-	cin>>n>>d>>k>>c;
-	for(int i=0;i<n;i++){
-		int yummy; cin>>yummy;
-		conveyer.push_back(yummy);
-	}
-	for(int i=0;i<k;i++){
-		if(sushi[conveyer[i]]++ == 0){
-			now++;
-		}
-	}
-	
-	for(int i=0;i<n;i++){
-		int tail = i;
-		int head = (i + k) % n;
-		if(--sushi[conveyer[tail]] == 0){
-			now--;
-		}
-		if(sushi[conveyer[head]]++ == 0){
-			now++;
-		}
-		MAX = max(MAX, now + !sushi[c]);
-	}
-	cout<<MAX;
-	return 0;
+    cin.tie(0) ->sync_with_stdio(0);
+    int n,d,k,c; cin>>n>>d>>k>>c;
+    vector<int> arr(n);
+    vector<int> sushi(d+1);
+    //sushi[i]: i초밥이 등장한 수
+   
+    int cnt=0, answer=0;
+    for(auto &i: arr) cin>>i;
+
+    for(int i=0;i<k;i++){
+        // sushi[arr[i]]++;
+        if(++sushi[arr[i]] == 1) cnt++;
+        answer = cnt;
+        if(sushi[c]==0) answer++;
+    }
+
+    for(int i=0;i<n;i++){
+        int l = i;//사라질 왼쪽
+        int r = (i+k) % n;//새로 들어올 오른쪽
+
+        
+        if(--sushi[arr[l]]==0) cnt--;
+        if(++sushi[arr[r]]==1) cnt++;
+        
+        if(sushi[c]==0) answer = max(answer, cnt + 1);
+        else answer = max(answer, cnt);
+    }
+
+    cout<<answer;
+    return 0;
 }
