90442cc75ce8f596063e394db734e49196912db9
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-03-20 15:50:49 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 19.27 ms, Memory: 28.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+// int dp[2500+5][2500+5];
+vector<vector<int>> dp(2550, vector<int>(2550, -1));
+int solution(string s){
+    int answer = 0;
+    for(int i=1;i<=s.size();i++){
+        for(int j=i-1;j<s.size();j++){
+            int l=j-i+1, r=j;
+            if(s[l] == s[r] && (r-l<=1 || dp[l+1][r-1] != -1)) dp[l][r] = r - l + 1;
+            answer = max(answer, dp[l][r]);
+        }
+    }
+    return answer;
+}
