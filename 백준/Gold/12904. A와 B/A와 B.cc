6a597dce2e8f51c8c74b759190c034442eb3c23a
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-04-19 13:50:47 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    string s,t;
+    cin>>s>>t;
+    int iter = t.size() - s.size();
+    while(iter--){
+        if(t.back() == 'A') t.pop_back();
+        else{
+            t.pop_back();
+            reverse(t.begin(), t.end());
+        }
+    }
+    if(s == t) cout<<1;
+    else cout<<0;
+    return 0;
+}
