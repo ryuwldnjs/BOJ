4f4d444b5ed735c3effa2d0e4a44f8d95f914956
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-05-31 15:29:38 +0900

    [Platinum V] Title: 중앙 트리, Time: 36 ms, Memory: 4132 KB -BaekjoonHub

---
+++ 백준/Platinum/7812. 중앙 트리/중앙 트리.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+struct Info{
+    ll b,cost;
+};
+vector<vector<Info>> graph;
+vector<ll> subtree, answer;
+vector<bool> visited;
+ll sum;//1번노드 기준최단거리 합
+ll n;
+//1번노드 기준 각각 서브트리 개수 저장
+ll dfs(int now){
+    visited[now] = true;
+
+    subtree[now] = 1;
+    for(auto [next, next_dist]: graph[now]){
+        if(!visited[next]){
+            subtree[now] += dfs(next);
+            sum += subtree[next] * next_dist;
+        }
+    }
+    return subtree[now];
+}
+
+
+void dfs2(int now, ll sum){
+    visited[now] = true;
+    answer[now] = sum;
+    for(auto [next, next_dist]: graph[now]){
+        if(!visited[next]){
+            ll next_sum = sum + next_dist * (n - subtree[next]*2);
+            dfs2(next, next_sum);
+        }
+    }
+}
+
+
+bool solve(){
+    cin>>n;
+    if(n == 0) return false;
+    graph = vector<vector<Info>>(n);
+    subtree = answer = vector<ll>(n);
+    visited = vector<bool>(n);
+    for(int i=0;i<n-1;i++){
+        int a,b,c;cin>>a>>b>>c;
+        graph[a].push_back({b,c});
+        graph[b].push_back({a,c});
+    }
+
+    sum = 0;
+    dfs(0);
+    visited = vector<bool>(n);
+    dfs2(0, sum);
+
+    cout<<*min_element(answer.begin(), answer.end())<<'\n';
+    return true;
+}
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    while(solve());
+    return 0;
+}
