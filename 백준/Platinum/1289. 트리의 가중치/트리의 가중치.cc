bfc4c83f9a0a60d4d6aa67212e49803d4967f60a
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-05-31 17:23:50 +0900

    [Platinum III] Title: 트리의 가중치, Time: 56 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Platinum/1289. 트리의 가중치/트리의 가중치.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+const ll MOD = 1'000'000'007;
+const ll MOD2 = MOD * 2;
+const ll INV2 = 500000004;  // 2의 역원 (mod 1e9+7)
+struct Info{
+    ll b,cost;
+};
+
+vector<vector<Info>> graph;
+vector<ll> subtree;
+vector<bool> visited;
+ll answer;
+ll dfs1(int now){
+    visited[now] = true;
+    for(auto [next, next_dist]: graph[now]){
+        if(visited[next]) continue;
+        
+        subtree[now] += (next_dist%MOD) * (dfs1(next) + 1);
+        subtree[now] %= MOD;
+    }
+    return subtree[now];
+}
+
+void dfs2(int now, ll sum){
+    visited[now] = true;
+    answer = (answer + sum + MOD) % MOD;
+    // printf("%d %lld, %d\n", now, sum, subtree[now]);
+    for(auto [next, next_dist]: graph[now]){
+        if(visited[next]) continue;
+        ll next_sum = sum - (next_dist%MOD) * (1+subtree[next]) + 1;
+        next_sum = (next_sum + MOD) % MOD;
+        next_sum *= next_dist;
+        next_sum = (next_sum + MOD) % MOD;
+        next_sum += subtree[next];
+        next_sum = (next_sum + MOD) % MOD;
+        dfs2(next, next_sum);
+    }
+}
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;cin>>n;
+    graph.resize(n+1);
+    subtree.resize(n+1);
+    for(int i=0;i<n-1;i++){
+        ll a,b,c;cin>>a>>b>>c;
+        graph[a].push_back({b,c});
+        graph[b].push_back({a,c});
+    }
+    visited = vector<bool>(n+1);
+    dfs1(1);
+    visited = vector<bool>(n+1);
+    dfs2(1, subtree[1]);
+
+    answer = answer * INV2 % MOD;
+
+    cout<<answer;
+    return 0;
+}
