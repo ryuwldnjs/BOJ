b349e4f67efe0b212a213b97332f479eabd7ec6f
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-06-30 16:39:44 +0900

    [Gold III] Title: 게임을 만들어요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/32299. 게임을 만들어요/게임을 만들어요.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;cin>>n;
+    if(n==3) cout<<"Hobanwoo";
+    else cout<<"Sangho";
+    return 0;
+}
