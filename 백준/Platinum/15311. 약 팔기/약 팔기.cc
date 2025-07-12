54417790fd08504455be8d407b14d8454c3da7a8
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-07-12 22:39:49 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;cin>>n;
+
+    cout<<"2000\n";
+    for(int i=0;i<1000;i++) cout<<"1 ";
+    for(int i=0;i<1000;i++) cout<<"1000 ";
+    return 0;
+}
