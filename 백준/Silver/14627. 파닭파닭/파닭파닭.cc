bdb53b706b0af5f1fac1a91e7040a191c140abfb
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-07-14 19:29:00 +0900

    [Silver II] Title: 파닭파닭, Time: 932 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+ll s,c;
+vector<ll> pa;
+bool able(ll x){
+    ll cnt = 0;
+    for(int i=0;i<s;i++){
+        cnt += pa[i] / x;
+    }
+    return cnt >= c;
+}
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    cin>>s>>c;
+    pa.resize(s);
+    for(int i=0;i<s;i++){
+        cin>>pa[i];
+    }
+
+    ll l=0, r=2e9;
+    for(int i=0;i<100;i++){
+        ll mid = (l+r)/2;
+        if(able(mid)) l = mid;
+        else r = mid;
+    }
+
+    ll total = accumulate(pa.begin(), pa.end(), 0LL);
+    if(able(r)) cout<<total - r * c;
+    else cout<<total - l * c;
+    return 0;
+}
