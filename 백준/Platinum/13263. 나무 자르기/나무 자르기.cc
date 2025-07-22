cda49865c79e4165eaf648d77be9ebbcff94c959
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-07-22 17:46:31 +0900

    [Platinum II] Title: 나무 자르기, Time: 20 ms, Memory: 4372 KB -BaekjoonHub

--- 백준/Platinum/13263. 나무 자르기/나무 자르기.cc
+++ 백준/Platinum/13263. 나무 자르기/나무 자르기.cc
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
-using pii=pair<int,int>;
-using pll=pair<ll,ll>;
-// f : ax + b
+const ll INF = 1e18;
+
+// 직선 f(x) = ax + b
 struct Line{
     ll a,b;
-    double start;
+    double start = -INF;
 
     bool operator<(const double x)const{
         return start <= x;
@@ -15,36 +15,38 @@ struct Line{
 
 //두 직선의 교점 x
 double cross(Line &f, Line &g){
-    return (g.b - f.b) / (f.a - g.a);
+    return (double)(g.b - f.b) / (f.a - g.a);
 }
 
-vector<Line> f;
-vector<ll> dp;
+// Convex Hull에 쌓이는 직선들
+vector<Line> hull;
 
 int main(){
     cin.tie(0) -> sync_with_stdio(0);
     int n;cin>>n;
-    dp.resize(n);
-    vector<ll> a(n), b(n);
+    vector<ll> a(n), b(n), dp(n);
+    
     for(int i=0;i<n;i++) cin>>a[i];
     for(int i=0;i<n;i++) cin>>b[i];
 
     for(int i=1;i<n;i++){
-        Line g = {b[i-1], dp[i-1], 0};
-        while(!f.empty()){
-            g.start = cross(f.back(), g);
-            if(f.back().start < g.start) break;
-            f.pop_back();
+        Line g = {b[i-1], dp[i-1]};
+
+        while(!hull.empty()){
+            g.start = cross(hull.back(), g);
+            if(hull.back().start < g.start) break;
+            hull.pop_back();
         }
-        f.emplace_back(g);
+        hull.emplace_back(g);
 
+        // x = a[i]에서의 최소값을 주는 직선 찾기
         ll x = a[i];
-        int idx = lower_bound(f.begin(), f.end(), x) - f.begin();
-        // if(idx>0) 
-        idx--;
+        int idx = lower_bound(hull.begin(), hull.end(), x) - hull.begin();
+        if(idx > 0) idx--;
 
-        dp[i] = f[idx].a * x + f[idx].b;
+        dp[i] = hull[idx].a * x + hull[idx].b;
     }
+    
     cout<<dp[n-1];
     return 0;
 }
