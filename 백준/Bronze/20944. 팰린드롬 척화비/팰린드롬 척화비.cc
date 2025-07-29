25c5c4d72097c768fdfc6917ad0b7ac845a047f8
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-07-29 22:26:24 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;cin>>n;
+    for(int i=0;i<n;i++) cout<<'a';
+    return 0;
+}
