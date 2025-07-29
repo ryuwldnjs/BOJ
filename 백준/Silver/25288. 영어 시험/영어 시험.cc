7af27c1cf7a2584ae7ac94c57343f3a196b593d9
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-07-29 22:16:18 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;string alpha;cin>>n>>alpha;
+
+    for(int i=0;i<n;i++) cout<<alpha;
+    return 0;
+}
