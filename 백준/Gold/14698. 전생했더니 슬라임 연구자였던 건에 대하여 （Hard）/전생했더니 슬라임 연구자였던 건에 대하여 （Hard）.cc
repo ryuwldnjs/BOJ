db93565041b90bfd9d4dd171bae422d21f698872
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-09-03 15:58:27 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 164 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+const ll MOD = 1000000007;
+
+void solve(){
+    int n;cin>>n;
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for(int i=0;i<n;i++){
+        ll x;cin>>x;
+        pq.push(x);
+    }
+
+    ll answer = 1;
+    while(pq.size() > 1){
+        ll a = pq.top(); pq.pop();
+        ll b = pq.top(); pq.pop();
+        answer *= a*b % MOD;
+        answer %= MOD;
+        pq.push(a*b);
+    }
+    cout<<answer<<'\n';
+}
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int t; cin>>t; while(t--) solve();
+    return 0;
+}
