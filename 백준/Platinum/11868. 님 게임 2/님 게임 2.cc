12cca6ead89504953caee49e1c8592595d9ffa03
류지 <69042677+ryuwldnjs@users.noreply.github.com>
2025-11-02 20:05:48 +0900

    [Platinum IV] Title: 님 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/11868. 님 게임 2/님 게임 2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+
+int main(){
+    cin.tie(0) -> sync_with_stdio(0);
+    int n;cin>>n;
+    
+    ll nim = 0;
+    for(int i=0;i<n;i++){
+        ll x;cin>>x;
+        nim ^= x;
+    }
+
+    if(nim) cout<<"koosaga";
+    else cout<<"cubelover";
+    return 0;
+}
